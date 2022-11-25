13b4f8130e56b5b3573f0141430acef5f7c9544f
camper2018 <Taniafali@gmail.com>
2022-11-25 10:31:02 -0800

    add modules to package.json

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 const app = express();
 app.use(cors());
 app.use(express.static(path.join(__dirname, "public")));
