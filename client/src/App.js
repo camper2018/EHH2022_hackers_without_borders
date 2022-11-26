c743ee6b7c5e958f63064c40149d7c10e38ec95d
camper2018 <Taniafali@gmail.com>
2022-11-25 21:25:06 -0800

    add home page

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,126 @@
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.css";
-
+import image from "./assets/profile-pic.png";
+import React, { useState, useEffect } from "react";
+import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import Login from "./component/Login";
+import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit<code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+  const [user, setUser] = useState(null);
+  const handleSignOut = () => {
+    localStorage.clear();
+    setUser(null);
+  };
+  useEffect(() => {
+    const username = localStorage.getItem("username");
+    const birthDate = localStorage.getItem("birthDate");
+    const address = localStorage.getItem("address");
+    setUser({ username, birthDate, address });
+  }, []);
+  if (user) {
+    return (
+      <div style={{ marginTop: "50px" }}>
+        <Button
+          className="btn-lg"
+          style={{ float: "right", marginRight: "30px" }}
+          onClick={handleSignOut}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          Sign out
+        </Button>
+        <center>
+          <h1>Welcome To Your Health Hub</h1>
+        </center>
+
+        <div>
+          <Card style={{ width: "40rem", margin: "0 auto" }}>
+            {/* <Card.Title>Your Profile</Card.Title> */}
+            <div style={{ display: "flex" }}>
+              <div>
+                <Card.Img
+                  variant="top"
+                  src={image}
+                  style={{ width: "220px", margin: "15px" }}
+                />
+
+                <Card.Body>
+                  <div>
+                    <Card.Text style={{ margin: "0 auto", padding: "10px" }}>
+                      <div
+                        style={{
+                          width: "200px",
+                          display: "flex",
+                          justifyContent: "space-around",
+                        }}
+                      >
+                        <b style={{ justifyContent: "flex-start" }}>Name:</b>
+                        <em>Mr. xyz</em>
+                      </div>
+                    </Card.Text>
+                    <Card.Text style={{ margin: "0 auto", padding: "10px" }}>
+                      <div
+                        style={{
+                          width: "200px",
+                          display: "flex",
+                          justifyContent: "space-around",
+                        }}
+                      >
+                        <b style={{ justifyContent: "flex-start" }}>DOB:</b>
+                        <em>01/01/2001</em>
+                      </div>
+                    </Card.Text>
+                    <Card.Text style={{ margin: "0 auto", padding: "10px" }}>
+                      <div
+                        style={{
+                          width: "200px",
+                          display: "flex",
+                          justifyContent: "space-around",
+                        }}
+                      >
+                        <b style={{ justifyContent: "flex-start" }}>Address:</b>
+                        <div>
+                          <div> Vlkova 5</div>
+                          <div>Praha 5</div>
+                          <div>150 00</div>
+                        </div>
+                      </div>
+                      <div style={{}}></div>
+                    </Card.Text>
+
+                    <Card.Text>
+                      <div></div>
+                    </Card.Text>
+                  </div>
+
+                  {/* <Button variant="primary">Go somewhere</Button> */}
+                </Card.Body>
+              </div>
+              <div
+                style={{
+                  padding: "10px",
+                  margin: "auto",
+                  fontSize: "20px",
+                }}
+              >
+                <h2>Our Mission</h2>
+                <div style={{ marginTop: "10px" }}>
+                  <p>To help you take confident decisions</p>
+                  <p>To become in charge of your own health</p>
+                </div>
+              </div>
+            </div>
+          </Card>
+        </div>
+      </div>
+    );
+  } else {
+    return (
+      <Container style={{ marginTop: "100px" }}>
+        <Login onLogin={setUser} />
+      </Container>
+    );
+  }
 }
 
 export default App;
---
+++ client/src/component/Login.js
@@ -0,0 +1,106 @@
+import React, { useState } from "react";
+import Card from "react-bootstrap/Card";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+function Login({ onLogin }) {
+  const [username, setUsername] = useState(null);
+  const [birthDate, setBirthDate] = useState(null);
+  const [address, setAddress] = useState({
+    street: "",
+    city: "",
+    country: "",
+    zipCode: "",
+  });
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    localStorage.setItem("username", username);
+    localStorage.setItem("birthDate", birthDate);
+    localStorage.setItem("address", address);
+    console.log("localStorage:", localStorage);
+    onLogin({ username, birthDate, address });
+  };
+  return (
+    <Card>
+      <h1 style={{ margin: "0 auto", marginTop: "5px" }}>Sign In</h1>
+      <Card.Body>
+        <Form>
+          <Form.Group className="mb-3" controlId="formUsername">
+            <Form.Label>Full Name</Form.Label>
+            <Form.Control
+              type="text"
+              onChange={(e) => {
+                setUsername(e.target.value);
+              }}
+            />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBirthDate">
+            <Form.Label>Date of Birth</Form.Label>
+            <Form.Control
+              type="date"
+              onChange={(e) => {
+                setBirthDate(e.target.value);
+              }}
+            />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formAddress">
+            <Form.Label className="mt-3">Address</Form.Label>
+            <Form.Control type="text" placeholder="Street Address" />
+            <Row>
+              <Col>
+                <Form.Label className="mt-3">City</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="City"
+                  onChange={(e) => {
+                    setAddress({ ...address, city: e.target.value });
+                  }}
+                />
+              </Col>
+              <Col>
+                <Form.Label className="mt-3">State</Form.Label>
+                <Form.Control
+                  placeholder="State"
+                  type="text"
+                  maxLength="2"
+                  onChange={(e) => {
+                    setAddress({ ...address, state: e.target.value });
+                  }}
+                />
+              </Col>
+              <Col>
+                <Form.Label className="mt-3">Country</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Country"
+                  onChange={(e) => {
+                    setAddress({ ...address, country: e.target.value });
+                  }}
+                />
+              </Col>
+              <Col>
+                <Form.Label className=" mt-3">Zip Code</Form.Label>
+
+                <Form.Control
+                  placeholder="ZIP"
+                  type="text"
+                  onChange={(e) => {
+                    setAddress({ ...address, zipCode: e.target.value });
+                  }}
+                />
+              </Col>
+            </Row>
+          </Form.Group>
+          <Button variant="primary" onClick={handleSubmit}>
+            Submit
+          </Button>
+        </Form>
+      </Card.Body>
+    </Card>
+  );
+}
+export default Login;
